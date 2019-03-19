25c5bf31bfbc798c64c299eb5b6bb34d6e8fd189
TestRookie <824427203@qq.com>
2019-03-19 18:07:03 +0800

    添加log日志

--- src/InterfaceTest/base/TestBase.java
+++ src/InterfaceTest/base/TestBase.java
@@ -17,24 +17,30 @@ public class TestBase {
     public int RESPNSE_STATUS_CODE_404 = 404;
     public int RESPNSE_STATUS_CODE_500 = 500;
 
-    public TestBase(){
-
+    public TestBase() {
 
         try {
-
             prop = new Properties();
-            FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"\\src\\InterfaceTest\\config\\config.properties");
-            FileInputStream fis1 = new FileInputStream(System.getProperty("user.dir")+"\\src\\config\\log4j.properties");
-
+            FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "\\src\\InterfaceTest\\config\\config.properties");
             prop.load(fis);
-            PropertyConfigurator.configure(fis1);
-        } catch(FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-        }catch (IOException e) {
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Properties props = new Properties();//创建一个系统参数对象
+            FileInputStream fis1 = new FileInputStream(System.getProperty("user.dir") + "\\src\\config\\log4j.properties");
+            props.load(fis1);//将配置加载到系统参数对象中
+            fis1.close();
+            PropertyConfigurator.configure(props);//装入log4j配置信息
+        }catch (IOException e){
             e.printStackTrace();
         }
 
     }
+
 
     @AfterSuite
     public void stopDriver() {
