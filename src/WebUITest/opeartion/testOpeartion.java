b597caf1be304a4ed3771410fe9a4269eb8f1657
TestRookie <824427203@qq.com>
2019-04-09 08:50:47 +0800

    WebUI测试

---
+++ src/WebUITest/opeartion/testOpeartion.java
@@ -0,0 +1,18 @@
+package WebUITest.opeartion;
+
+import WebUITest.base.WebUI;
+import org.openqa.selenium.WebDriver;
+
+import java.io.FileNotFoundException;
+
+public class testOpeartion extends WebUI {
+
+    public testOpeartion(WebDriver driver) {
+        super(driver);
+    }
+
+    public void testBaiduClick() throws FileNotFoundException {
+        click(getElement("baidu1"));
+    }
+
+}
